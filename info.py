8e09a32637ab31a2ff397cb92c81da6c038151c2
Soledad <soledadbarrios@gmail.com>
2021-08-06 22:56:39 -0300

    segundocommit

---
+++ info.py
@@ -0,0 +1,11 @@
+import time
+def mostrar_bienvenida():
+	print('''
+	                 +++++++++++++++++++++++++++++++++++++++++++++++++++
+                     +                                                 +
+                     +      BIENVENIDOS AL JUEGO SOPA DE LETRAS        +
+                     +                                                 +  
+                     +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+         ''')
+mostrar_bienvenida()
